5b53f01805d62d9db4e8c0164ba2dc7f6c9a9199
alejandraob <alejandraboisier@hotmail.com>
2016-11-24 23:25:36 -0300

    cambios 24/11 ale

--- js/mapmarker_func.jquery.js
+++ js/mapmarker_func.jquery.js
@@ -1,13 +1,14 @@
 		//set up markers 
 		var myMarkers = {"markers": [
-				{"latitude": "51.511732", "longitude":"-0.123270", "icon": "img/map-marker.png"}
+				{"latitude": "-37.8799184", "longitude":"-67.7960689", "icon": "img/map-marker.png"}
+
 			]
 		};
 		
 		//set up map options
 		$("#map").mapmarker({
 			zoom	: 14,
-			center	: 'Covent Garden London',
+			center	: 'La Pampa 489, 8307 Catriel, Río Negro',
 			markers	: myMarkers
 		});
 
